e3313a56390e16d26123fe2fb637d189d35a1ef4
Jason <jason.bundy@me.com>
2023-01-27 15:04:40 -0500

    initial commit - something to come back to

--- js/main.js
+++ js/main.js
@@ -1,1 +1,34 @@
-// Write your DOM code here!
+const red = document.querySelector(".red");
+const white = document.querySelector(".white");
+const blue = document.querySelector(".blue");
+const yellow = document.querySelector(".yellow");
+const body = document.getElementsByTagName("body")[0];
+
+const makeRed = () => {
+  body.className = "";
+  body.style.backgroundColor = "red";
+  body.style.color = "white";
+};
+
+const makeWhite = () => {
+  body.className = "";
+  body.style.backgroundColor = "white";
+  body.style.color = "black";
+};
+
+const makeBlue = () => {
+  body.className = "";
+  body.style.backgroundColor = "blue";
+  body.style.color = "white";
+};
+
+const makeYellow = () => {
+  body.className = "";
+  body.style.backgroundColor = "yellow";
+  body.style.color = "black";
+};
+
+red.addEventListener("click", makeRed);
+white.addEventListener("click", makeWhite);
+blue.addEventListener("click", makeBlue);
+yellow.addEventListener("click", makeYellow);
